ffaec1d4c0709832be4e8be9d34781244aa858c9
Liuyue Jiang <48501982+liuyuejiang@users.noreply.github.com>
2019-07-10 18:04:16 +0930

    Update select_usedcar_detail.php

--- usedcar_detail/select_usedcar_detail.php
+++ usedcar_detail/select_usedcar_detail.php
@@ -7,7 +7,7 @@ $start = ($page-1)*$display;
 $queryResult = $connect->query("SELECT usedcar_autobrandid, usedcar_class, usedcar_name, usedcar_detail, usedcar_price, usedcar_yprice, usedcar_year, usedcar_lt,
 usedcar_ltl, usedcar_amt, usedcar_register, usedcar_mileage, usedcar_colour, usedcar_add, usedcar_author, usedcar_tal, usedcar_sub, usedcar_display, usedcar_video,
 usedcar_pic1, usedcar_pic2, usedcar_pic3, usedcar_pic4, usedcar_pic5, usedcar_pic6, usedcar_pic7, usedcar_pic8, usedcar_pic9, usedcar_pic10
-FROM usedcar_detail WHERE usedcar_display = 1 AND usedcar_name = $name LIKE '%{$name}%' Order By usedcar_time Desc LIMIT $start,$display");
+FROM usedcar_detail WHERE usedcar_display = 1 AND usedcar_name LIKE '%{$name}%' Order By usedcar_time Desc LIMIT $start,$display");
 $result = array();
 while ( $fetchData = $queryResult -> fetch_assoc()){
 $result [] = $fetchData;
